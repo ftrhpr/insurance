ed1b39f700414073773b780399359b507846ce9a
ftrhpr <N.bikashvili88@gmail.com>
2025-12-05 22:01:11 +0400

    fx

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 // Error handling configuration
 error_reporting(E_ALL);
-ini_set('display_errors', 1); // Temporarily enabled to debug login error
+ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/error_log');
 
