66a14d4f46df9d92af300f2f3f77033045bcf545
ftrhpr <N.bikashvili88@gmail.com>
2025-12-08 01:48:14 +0400

    fx

--- api.php
+++ api.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require_once 'session_config.php';
 
 header("Content-Type: application/json");
