2ad249058e4b0e2d685ea3845ebbf11f6c8f935e
ftrhpr <N.bikashvili88@gmail.com>
2026-01-17 00:24:38 +0400

    fx

--- technician_dashboard.php
+++ technician_dashboard.php
@@ -38,7 +38,7 @@ if (isset($_GET['json'])) {
         $statuses = json_decode($c['stage_statuses'] ?? '{}', true);
         $timers = json_decode($c['stage_timers'] ?? '{}', true);
         foreach ($assignments as $stage => $techId) {
-            if (intval($techId) === intval($userId) && ($statuses[$stage] ?? null) !== 'finished' && !empty($timers[$stage])) {
+            if (intval($techId) === intval($userId)) {
                 $assigned[] = [
                     'id' => $c['id'],
                     'plate' => $c['plate'],
@@ -68,7 +68,7 @@ foreach ($cases as $c) {
     $statuses = json_decode($c['stage_statuses'] ?? '{}', true);
     $timers = json_decode($c['stage_timers'] ?? '{}', true);
     foreach ($assignments as $stage => $techId) {
-        if (intval($techId) === intval($userId) && ($statuses[$stage] ?? null) !== 'finished' && !empty($timers[$stage])) {
+        if (intval($techId) === intval($userId)) {
             $assigned[] = [
                 'id' => $c['id'],
                 'plate' => $c['plate'],
