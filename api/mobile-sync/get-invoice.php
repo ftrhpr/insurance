f42f6099fcdb634aa4342fb5994c25bbf04ab3fd
ftrhpr <N.bikashvili88@gmail.com>
2026-01-31 14:20:59 +0400

    fx

--- api/mobile-sync/get-invoice.php
+++ api/mobile-sync/get-invoice.php
@@ -139,7 +139,7 @@ try {
         'caseType' => $invoice['case_type'] ?? null,
         'assigned_mechanic' => $invoice['assigned_mechanic'] ?? null,
         'assignedMechanic' => $invoice['assigned_mechanic'] ?? null,
-        'nachrebi_qty' => isset($invoice['nachrebi_qty']) ? intval($invoice['nachrebi_qty']) : null,
+        'nachrebi_qty' => isset($invoice['nachrebi_qty']) ? floatval($invoice['nachrebi_qty']) : null,
         'status_id' => isset($invoice['status_id']) ? intval($invoice['status_id']) : null,
         'statusId' => isset($invoice['status_id']) ? intval($invoice['status_id']) : null,
         'repair_status_id' => isset($invoice['repair_status_id']) ? intval($invoice['repair_status_id']) : null,
